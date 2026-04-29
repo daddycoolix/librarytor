0f7daf7d794e08fd2130827d951961472da0ff79
daddycoolix <73852574+daddycoolix@users.noreply.github.com>
2026-04-29 07:36:57 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,18 +1,22 @@
-const CACHE='street-libs-v36';
-const ASSETS=['./','/index.html','/manifest.json'];
+// V44.01 - מספר הגרסה חייב להשתנות בכל עדכון
+const CACHE = 'librarytor-v44.02';
+const ASSETS = ['./', '/index.html', '/manifest.json', '/libraries_data.js', '/books_data.js'];
 
-self.addEventListener('install',e=>{
-  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
   self.skipWaiting();
 });
 
-self.addEventListener('activate',e=>{
-  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
   self.clients.claim();
 });
 
-self.addEventListener('fetch',e=>{
-  // תמיד בדוק מהרשת קודם, cache רק כגיבוי
+self.addEventListener('fetch', e => {
   e.respondWith(
     fetch(e.request)
       .then(r => {
